bead1c54193457f0d7d67ba1b3c8d904126ca525
chaharhimanshu1004 <chaharhimanshu03@gmail.com>
2024-07-15 00:55:12 +0530

    styling players

--- components/Player/index.js
+++ components/Player/index.js
@@ -1,9 +1,15 @@
 import ReactPlayer from 'react-player'
+import cx from 'classnames'
+import styles from '@/components/Player/index.module.css'
 const Player = (props) =>{
-    const {url,muted,playing} = props;
+    const {url,muted,playing,isHighlighted} = props;
     return (
-        <div>
-            <ReactPlayer url={url} muted={muted} playing={playing}/>
+        <div className={cx(styles.playerContainer,{
+            [styles.notActive] : !isHighlighted,
+            [styles.active] : isHighlighted,
+
+        })}>
+            <ReactPlayer url={url} muted={muted} playing={playing} width="100%" height="100%" />
         </div>
     )
 
--- pages/[roomId].js
+++ pages/[roomId].js
@@ -1,84 +1,93 @@
-
 import { useSocket } from "@/context/socket";
 import { useEffect } from "react";
-import usePeer from '@/hooks/usePeer'
+import usePeer from "@/hooks/usePeer";
 import useMediaStream from "@/hooks/useMediaStream";
 import Player from "../components/Player";
 import usePlayer from "../hooks/usePlayer";
+import styles from "@/styles/room.module.css"
 const Room = () => {
-    const socket = useSocket();
-    const {peer,myId} = usePeer();
-    const {stream} = useMediaStream();
-    const {players,setPlayers} = usePlayer(myId); // why didn't we get the myId by calling hook, -- >>bcz hooks create a new instance whenever they are created and hence same myId ni rehti phir -- > can fix this by creating a global context
+  const socket = useSocket();
+  const { peer, myId } = usePeer();
+  const { stream } = useMediaStream();
+  const { players, setPlayers , playerHighlighted,nonHighlightedPlayers} = usePlayer(myId); // why didn't we get the myId by calling hook, -- >>bcz hooks create a new instance whenever they are created and hence same myId ni rehti phir -- > can fix this by creating a global context
 
-    useEffect(()=>{
-        if(!socket || !peer || !stream)return;
-        const handleUserConnected = (newUser)=>{
-            console.log(`a new user connected with userId ${newUser}`)
-            const call = peer.call(newUser,stream);
-            call.on('stream',(incomingStream)=>{
-                console.log(`incoming stream from ${newUser}`)
-                setPlayers((prev)=>({
-                    ...prev,
-                    [newUser]:{
-                        url:incomingStream,
-                        muted : false,
-                        playing : true,
-                    }
-                }))
-            })
-        }
-        socket.on('user-connected',handleUserConnected);
-        return () =>{
-            socket.off('user-connected',handleUserConnected);
-        }
-    },[peer,setPlayers,socket,stream])
+  useEffect(() => {
+    if (!socket || !peer || !stream) return;
+    const handleUserConnected = (newUser) => {
+      console.log(`a new user connected with userId ${newUser}`);
+      const call = peer.call(newUser, stream);
+      call.on("stream", (incomingStream) => {
+        console.log(`incoming stream from ${newUser}`);
+        setPlayers((prev) => ({
+          ...prev,
+          [newUser]: {
+            url: incomingStream,
+            muted: false,
+            playing: true,
+          },
+        }));
+      });
+    };
+    socket.on("user-connected", handleUserConnected);
+    return () => {
+      socket.off("user-connected", handleUserConnected);
+    };
+  }, [peer, setPlayers, socket, stream]);
 
-    useEffect(()=>{
-        if(!peer || !stream)return;
-        peer.on('call',(call)=>{
-            const {peer : callerId} = call; // caller ki id le liya -- peer naam se ni lera coz peer pehle se upr define, uska naam change krke callerId kr diya
-            call.answer(stream); // send back your stream when you answer the call
-            call.on('stream',(incomingStream)=>{
-                console.log(`incoming stream from ${callerId}`)
-                setPlayers((prev)=>({
-                    ...prev,
-                    [callerId]:{
-                        url:incomingStream,
-                        muted : false,
-                        playing : true,
-                    }
-                }))
-            })
-        })
+  useEffect(() => {
+    if (!peer || !stream) return;
+    peer.on("call", (call) => {
+      const { peer: callerId } = call; // caller ki id le liya -- peer naam se ni lera coz peer pehle se upr define, uska naam change krke callerId kr diya
+      call.answer(stream); // send back your stream when you answer the call
+      call.on("stream", (incomingStream) => {
+        console.log(`incoming stream from ${callerId}`);
+        setPlayers((prev) => ({
+          ...prev,
+          [callerId]: {
+            url: incomingStream,
+            muted: false,
+            playing: true,
+          },
+        }));
+      });
+    });
+  }, [peer, setPlayers, stream]);
 
-    },[peer,setPlayers,stream])
+  useEffect(() => {
+    if (!stream || !myId) return;
+    console.log(`setting my stream ${myId}`);
+    setPlayers((prev) => ({
+      ...prev,
+      [myId]: {
+        url: stream,
+        muted: false,
+        playing: true,
+      },
+    }));
+  }, [myId, setPlayers, stream]);
 
-    useEffect(()=>{
-        if(!stream || !myId )return;
-        console.log(`setting my stream ${myId}`)
-        setPlayers((prev)=>({
-            ...prev,
-            [myId]:{
-                url:stream,
-                muted : false,
-                playing : true,
-            }
-        }))
-
-    },[myId,setPlayers,stream]);
-
-
-    return (
-        <div>
-            {
-                Object.keys(players).map((playerId)=>{
-                    const {url,muted,playing} = players[playerId];
-                    return <Player key={playerId} url={url} muted={muted} playing={playing} />
-                })
-            }
-        </div>
-    )
-}
+  return (
+    <>
+      <div className={styles.activePlayerContainer}>
+        {playerHighlighted && (
+          <Player
+            url={playerHighlighted.url}
+            muted={playerHighlighted.muted}
+            playing={playerHighlighted.playing}
+            isHighlighted
+          />
+        )}
+      </div>
+      <div className={styles.inActivePlayerContainer}>
+        {Object.keys(nonHighlightedPlayers).map((playerId) => {
+          const { url, muted, playing } = nonHighlightedPlayers[playerId];
+          return (
+            <Player key={playerId} url={url} muted={muted} playing={playing} isHighlighted={false} />
+          );
+        })}
+      </div>
+    </>
+  );
+};
 
 export default Room;
